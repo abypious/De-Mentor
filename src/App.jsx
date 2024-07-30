2230c8f4275d0ff93fccceebfae9f1a80eb56ae7
abnormaldil <abnormaldil.official@gmail.com>
2024-07-31 00:33:28 +0530

    demo push

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="Login.jsx" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
