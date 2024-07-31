342a9dc77a827e69f46eed14b69b41c8566f709a
abnormaldil <abnormaldil.official@gmail.com>
2024-08-01 02:19:44 +0530

    Added homepage and new ui to login

---
+++ src/constants/indexlogin.jsx
@@ -0,0 +1,9 @@
+export const navItems = [
+    { label: "Quit Smoking", href: "QuitSmoking" },
+    { label: "Quit Drinking", href: "Drink.Jsx" },
+    { label: "Quit Screen", href: "Porn.jsx" },
+    { label: "Quit Porn", href: "Porn.jsx" },
+    { label: "Contact Us", href: "Contact" },
+  ];
+  
+  
